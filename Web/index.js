eb984d08a69612a2d7d3680ef4d3115bd7999083
Jimmy Klein <james.klein@temple.edu>
2017-04-25 20:02:35 -0400

    Added post method for tagging and changed so you dont need every field to update

--- Web/index.js
+++ Web/index.js
@@ -6,6 +6,8 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//add post request for tagging a user
+
 var db;
 MongoClient.connect('mongodb://127.0.0.1:27017/zombiesvshumans', (err, database)=>{
   if(err) return console.log(err);
@@ -34,8 +36,6 @@ app.get('/getusers', (req, res) =>{
 app.post('/updateuser', (req, res) =>{
   var cursor = db.collection('users').find({username: req.body.username}).toArray(function(err, results){
     if(err) return (console.log(err));
-    //If the user doesn't already exist, insert it
-    console.log(req.body);
     if(results.length==0){
       db.collection('users').save(req.body, (err, results)=>{
         if(err) return (console.log(err));
@@ -43,19 +43,46 @@ app.post('/updateuser', (req, res) =>{
     } else {
       //If the user does exist, update the entry. Prob better way to do this
       console.log(req.body.lat!=null)
-      if(req.body.username!=null && req.body.long!=null && req.body.lat!=null && req.body.iszombie!=null && req.body.lastupdated!=null ){
-        db.collection('users').update({username: req.body.username}, {username:req.body.username, long:req.body.long, lat:req.body.lat, iszombie:req.body.iszombie, lastupdated:req.body.lastupdated});
-
+      //if(req.body.username!=null && req.body.long!=null && req.body.lat!=null && req.body.iszombie!=null && req.body.lastupdated!=null ){
+        //db.collection('users').update({username: req.body.username}, {username:req.body.username, long:req.body.long, lat:req.body.lat, iszombie:req.body.iszombie, lastupdated:req.body.lastupdated});
+        console.log(req.body.iszombie);
+        if(req.body.long!=null){
+          db.collection('users').update({username: req.body.username}, { $set: {iszombie:req.body.long } });
+        }
+        if(req.body.lat!=null){
+          db.collection('users').update({username: req.body.username}, { $set: {iszombie:req.body.lat } });
+        }
+        if(req.body.iszombie!=null){
+          db.collection('users').update({username: req.body.username}, { $set: {iszombie:req.body.iszombie } });
+        }
+        if(req.body.lastupdated!=null){
+          db.collection('users').update({username: req.body.username}, { $set: {iszombie:req.body.lastupdated } });
+        }
+        var dbSize;
+        db.collection('users').count((err, results)=>{
+          if(err) return (console.log(err));
+          dbSize = results;
+        });
         db.collection('users').count({iszombie:'true'}, (err, result)=>{
-          if(result>= db.collection('users')){
+          if(err) return (console.log(err));
+          if(result==dbSize){
             //remix
           }
         });
-      }
+      //}
     }
   });
   res.end('yes');
-  //Not sure if need
-  //var isZombie = req.query.isZombie;
+  //change this to got tagged for tagged, and new game for new game
+});
 
+
+app.post('/taguser', (req, res) =>{
+  db.collection('users').find({username: req.body.username}).toArray((err, results)=>{
+    if(err) return (console.log(err));
+    if(results.length>0){
+      if(req.body.iszombie!=null){
+        db.collection('users').update({username: req.body.username}, { $set: {iszombie:"true" } });
+      }
+  })
 });
