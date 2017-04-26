097c1378a069dda2ad311916a6414521f44a6b8f
JoshDronyi <joshuadronyi@gmail.com>
2017-04-26 05:14:30 -0400

    more login changes - without all audio

--- Zombies-vs-Humans/app/src/main/java/com/example/mangaramu/zombies_vs_humans/SignuploginAct.java
+++ Zombies-vs-Humans/app/src/main/java/com/example/mangaramu/zombies_vs_humans/SignuploginAct.java
@@ -2,18 +2,27 @@ package com.example.mangaramu.zombies_vs_humans;
 
 
 import android.app.Activity;
+import android.bluetooth.BluetoothClass;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.content.res.Resources;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Debug;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.error.ANError;
@@ -28,10 +37,12 @@ import org.json.JSONObject;
 public class SignuploginAct extends Activity {
 
     EditText playernmae;
+    TextView graveText;
     Button play;
     SharedPreferences sharedpref;
     SharedPreferences.Editor editor;
     String LINK;
+    MediaPlayer cackleSound;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -43,15 +54,53 @@ public class SignuploginAct extends Activity {
         sharedpref = getPreferences(Context.MODE_PRIVATE);
         editor = sharedpref.edit();
         LINK = getResources().getString(R.string.URL);
+        //sounds
+        cackleSound = MediaPlayer.create(this,R.raw.cackle3);
 
         if (sharedpref.getString("Name", "").equals("")) {
+
             playernmae = (EditText) findViewById(R.id.playername);
-            play = (Button) findViewById(R.id.playbutt);
+            play = (Button) findViewById(R.id.playbutt);//Tee hee, you said butt
+            graveText = (TextView) findViewById(R.id.graveText);
+
+
+            //Finds the dimensions of the devices windows and stores them in height and width variables
+            DisplayMetrics  metrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(metrics);
+            final int screenWidth =metrics.widthPixels;
+            final int screenHeight = metrics.heightPixels;
+
+            //For animating the grave writing
+            playernmae.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                    if(screenWidth > 1023 && screenHeight > 1023){ //Screen is probably a tablet
+                        graveText.setText("R.I.P \n \n \n " +String.valueOf(s));
+                    }
+                    else{ //Screen is probably a phone
+                        graveText.setText("R.I.P \n " +String.valueOf(s));
+                    }
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+
+                }
+            });
+
+            //Click listener for the play button
             play.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     final String name;
                     name = playernmae.getText().toString();
+                    cackleSound.start();
 
                     Log.d("Name", name);
 
@@ -142,6 +191,7 @@ public class SignuploginAct extends Activity {
 
     public void StartGame(String name)// Takes in a string called name. Returns null. Will create an intent to start the GameActivity while also putting the inserted string as an extra to the application.
     {
+
         Intent start = new Intent(this, GameActivity.class);
         start.putExtra("Username", name);
         startActivity(start);
